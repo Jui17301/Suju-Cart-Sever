04b01b19980012cd91041ce5c7a07a121ec6e7e5
Jui <cse_1912020087@gmail.com>
2024-11-06 01:42:17 +0600

    app.ts updated

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000',
+      'https://suju-cart.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
